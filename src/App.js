f13e553fcb9f88bf1f04d049c24764005e27b0bb
Naa-A <61292016+Naa-A@users.noreply.github.com>
2024-01-23 00:33:51 +0000

    Weather component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App This project was coded by Kai and is
-        <a href="https://github.com/Naa-A/she-codes-weather-app-week-five">
-          open sourced on Github
-        </a>
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          Weather App This project was coded by Kai and is
+          <a
+            href="https://github.com/Naa-A/she-codes-weather-app-week-five"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
